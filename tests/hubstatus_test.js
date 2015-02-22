3b99da8d16fcd947ac09178f36b034c5d65248d9
Sergey Belov <peimei@ya.ru>
2015-02-22 09:51:05 +0300

    Make hubstatus_test work

--- tests/hubstatus_test.js
+++ tests/hubstatus_test.js
@@ -1,25 +1,26 @@
-var server = require('../server.js');
-var should = require('should');
-var fs = require('fs');
+var server = require('../server'),
+    should = require('should'),
+    path = require('path'),
+    fs = require('fs'),
+    request = require('supertest');
 
-var request = require('supertest');
 describe('Server', function() {
 	describe('GET /grid/api/hub', function() {
 		it('responds with the hub configuration', function(done) {
-			fs.readFile('../config.json', function(err, data) {
-			  request(server)
-				.get('/grid/api/hub/')
-				.end(function(err, res) {
-					var config = JSON.parse(data.toString());
-					res.statusCode.should.equal(200);
-					
-					res.body.should.be.an.instanceOf(Object)
-					res.body.port.should.equal(config.port);
-					res.body.host.should.equal(config.host);
+            fs.readFile(path.join(__dirname, '..', 'config.json'), function (err, data) {
+                request(server())
+                    .get('/grid/api/hub/')
+                    .end(function (err, res) {
+                        var config = JSON.parse(data.toString());
+                        res.statusCode.should.equal(200);
 
-					done();
-				});
-			});
+                        res.body.should.be.an.instanceOf(Object);
+                        res.body.port.should.equal(config.port);
+                        res.body.host.should.equal(config.host);
+
+                        done();
+                    });
+            });
 		});
 	});
 });
