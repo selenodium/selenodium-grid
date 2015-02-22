3798627de8cf45d55e12bcf22ff4325446c99ab4
Sergey Belov <peimei@ya.ru>
2015-02-22 16:55:14 +0300

    Make status_test work

--- tests/status_test.js
+++ tests/status_test.js
@@ -1,17 +1,25 @@
-var server = require('../server');
-var should = require('should');
-
-var request = require('supertest');
+var server = require('../server'),
+    should = require('should'),
+    request = require('supertest');
 
 describe('Status', function() {
+
+    var app;
+    before(function() {
+        app = server();
+    });
+
+    after(function() {
+        app.close();
+    });
+
 	describe('GET /grid/api/proxy', function() {
 		it('responds with an unknown node if the node is polling this without registering first', function(done) {
-		  request(server)
+		  request(app)
 			.get('/grid/api/proxy?id=http://10.0.0.1:5554')
 			.end(function(err, res) {
-				var body = res.body;
-				res.body.should.be.an.instanceOf(Object)
-				res.body.success.should.be.false
+				res.body.should.be.an.instanceOf(Object);
+				res.body.success.should.be.false;
 				done();
 			});
 		});
@@ -21,7 +29,7 @@ describe('Status', function() {
 		var postData = '{"class":"org.openqa.grid.common.RegistrationRequest","capabilities":[{"platform":"WINDOWS","seleniumProtocol":"Selenium","browserName":"iexplore","maxInstances":1,"version":"9"}],"configuration":{"port":5560,"nodeConfig":"config.json","host":"10.0.1.15","cleanUpCycle":10000,"browserTimeout":20000,"hubHost":"10.0.1.6","registerCycle":5000,"debug":"","hub":"http://10.0.1.6:4444/grid/register","log":"test.log","url":"http://10.0.1.15:5560","remoteHost":"http://10.0.1.15:5560","register":true,"proxy":"org.openqa.grid.selenium.proxy.DefaultRemoteProxy","maxSession":1,"role":"node","hubPort":4444}}';
 
 		beforeEach(function(d) {
-			request(server)
+			request(app)
 				.post('/grid/register')
 				.send(postData)
 				.end(function(err, res) {
@@ -32,12 +40,11 @@ describe('Status', function() {
 		});
 
 		it('responds with an ok if the node polling has been registered before', function(done) {
-		  	request(server)
+		  	request(app)
 				.get('/grid/api/proxy?id=http://10.0.1.15:5560')
 				.end(function(err, res) {
-					var body = res.body;
-					res.body.should.be.an.instanceOf(Object)
-					res.body.success.should.be.true
+					res.body.should.be.an.instanceOf(Object);
+					res.body.success.should.be.true;
 					done();
 				});
 		});
@@ -47,7 +54,7 @@ describe('Status', function() {
 		var postData = '{"class":"org.openqa.grid.common.RegistrationRequest","capabilities":[{"platform":"WINDOWS","seleniumProtocol":"Selenium","browserName":"iexplore","maxInstances":1,"version":"9"}],"configuration":{"port":5560,"nodeConfig":"config.json","host":"10.0.1.16","cleanUpCycle":10000,"browserTimeout":20000,"hubHost":"10.0.1.6","registerCycle":5000,"debug":"","hub":"http://10.0.1.6:4444/grid/register","log":"test.log","url":"http://10.0.1.16:5560","remoteHost":"http://10.0.1.16:5560","register":true,"proxy":"org.openqa.grid.selenium.proxy.DefaultRemoteProxy","maxSession":1,"role":"node","hubPort":4444}}';
 
 		beforeEach(function(d) {
-			request(server)
+			request(app)
 				.post('/grid/register')
 				.send(postData)
 				.end(function(err, res) {
@@ -60,12 +67,11 @@ describe('Status', function() {
 		it('should return a not found response if the node has not responded in the nodeTimeout time', function(done) {
 			this.timeout(20000);
 			setTimeout(function() {
-				request(server)
+				request(app)
 				.get('/grid/api/proxy?id=http://10.0.1.16:5560')
 				.end(function(err, res) {
-					var body = res.body;
-					res.body.should.be.an.instanceOf(Object)
-					res.body.success.should.be.false
+					res.body.should.be.an.instanceOf(Object);
+					res.body.success.should.be.false;
 					done();
 				});
 			}, 16000);
@@ -74,13 +80,12 @@ describe('Status', function() {
 		it('should be possible to register the node again after it has failed to respond in nodeTimeout time', function(done) {
 			this.timeout(20000);
 			setTimeout(function() {
-				request(server)
+				request(app)
 				.get('/grid/api/proxy?id=http://10.0.1.16:5560')
 				.end(function(err, res) {
-					var body = res.body;
-					res.body.should.be.an.instanceOf(Object)
-					res.body.success.should.be.false
-					request(server)
+					res.body.should.be.an.instanceOf(Object);
+					res.body.success.should.be.false;
+					request(app)
 						.post('/grid/register')
 						.send(postData)
 						.end(function(err, res) {
