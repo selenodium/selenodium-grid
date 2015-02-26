c65e58949660c5789c0692d3b56a6abc8b2f08b2
Sergey Belov <peimei@ya.ru>
2015-02-26 13:07:01 +0300

    Rewrite capabilitymatcher_test from should to must

--- tests/capabilitymatcher_test.js
+++ tests/capabilitymatcher_test.js
@@ -1,138 +1,133 @@
-var matcher = require('../lib/capabilitymatcher');
-
-var http = require('http');
-var should = require('should');
-var request = require('supertest');
-var assert = require('assert');
-var models = require('../lib/models');
-var registry = require('../lib/registry');
-var store = require('../lib/store');
+var expect = require('must'),
+    matcher = require('../lib/capabilitymatcher'),
+    models = require('../lib/models'),
+    store = require('../lib/store');
 
 describe('CapabilityMatcher', function() {
-	describe('Correctly find a match', function() {
-		beforeEach(function(done) {
-			store.removeAllAvailableNodes(done);
-		});
+    describe('Correctly find a match', function() {
+        beforeEach(function(done) {
+            store.removeAllAvailableNodes(done);
+        });
 
-		it('should correctly find a full match', function(done) {
-			// Store capabilities keys should be all in lower case
+        it('must correctly find a full match', function(done) {
+            // Store capabilities keys should be all in lower case
             var caps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'};
 
             store.addAvailableNode(new models.Node('127.0.0.1', 5556, [caps]), function() {
-				matcher.findNode(caps, function(node) {
-					node.host.should.equal('127.0.0.1');
-					done();
-				}, true);
-			});
-		});
+                matcher.findNode(caps, function(node) {
+                    expect(node.host).to.equal('127.0.0.1');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should find a version even if it\'s a string', function(done) {
-			var storeCaps = {browsername: 'firefox', version: '14', platform: 'WINDOWS'},
+        it('must find a version even if it\'s a string', function(done) {
+            var storeCaps = {browsername: 'firefox', version: '14', platform: 'WINDOWS'},
                 findCaps = {browserName: 'firefox', version: 14, platform: 'WINDOWS'};
 
             store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode(findCaps, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+                matcher.findNode(findCaps, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should be case-insensitive', function(done) {
+        it('must be case-insensitive', function(done) {
             var storeCaps = {browsername: 'firefox', version: '14', platform: 'WINDOWS'},
                 findCaps = {browserName: 'FIREFOX', Version: '14', platform: 'windows'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode(findCaps, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode(findCaps, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should not crash when the user asks for a permission which is not registered on the node', function(done) {
+        it('must not crash when the user asks for a permission which is not registered on the node', function(done) {
             var storeCaps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'},
                 findCaps = {browserName: 'firefox', version: 14, platform: 'WINDOWS', cherries: 'ontop'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode(findCaps, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode(findCaps, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should be able to handle empty desired capabilities', function(done) {
+        it('must be able to handle empty desired capabilities', function(done) {
             var storeCaps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode({}, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode({}, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should be able to handle incorrect capabilities', function(done) {
+        it('must be able to handle incorrect capabilities', function(done) {
             var storeCaps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode({nothing: 'else'}, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode({nothing: 'else'}, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should correctly find a node when the user asks for ANY OS', function(done) {
+        it('must correctly find a node when the user asks for ANY platform', function(done) {
             var storeCaps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'},
                 findCaps = {browserName: 'firefox', version: 14, platform: 'ANY'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode(findCaps, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode(findCaps, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should correctly find a node when the user asks for ANY OS', function(done) {
+        it('must correctly find a node when the user asks for * platform', function(done) {
             var storeCaps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'},
                 findCaps = {browserName: 'firefox', version: 14, platform: '*'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode(findCaps, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode(findCaps, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should correctly find a node with just one desired capability', function(done) {
+        it('must correctly find a node with just one desired capability', function(done) {
             var storeCaps = {browsername: 'firefox', version: 14, platform: 'WINDOWS'},
                 findCaps = {browserName: 'firefox'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
-				matcher.findNode(findCaps, function(node) {
-					node.host.should.equal('127.0.0.2');
-					done();
-				}, true);
-			});
-		});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps]), function() {
+                matcher.findNode(findCaps, function(node) {
+                    expect(node.host).to.equal('127.0.0.2');
+                    done();
+                }, true);
+            });
+        });
 
-		it('should pick the correct node that corresponds to our desired_capabilities', function(done) {
+        it('must pick the correct node that corresponds to our desiredCapabilities', function(done) {
             var storeCaps1 = {browsername: 'firefox', version: 14, platform: 'WINDOWS'},
                 storeCaps2 = {browsername: 'firefox', version: 13, platform: 'LINUX'},
                 findCaps = {browserName: 'firefox', platform: 'LINUX'};
 
-			store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps1]), function() {
-				store.addAvailableNode(new models.Node('127.0.0.3', 5556, [storeCaps2]), function() {
-					matcher.findNode(findCaps, function(node) {
-						node.host.should.equal('127.0.0.3');
-						done();
-					}, true);
-				});
-			});
-		});
-	});
+            store.addAvailableNode(new models.Node('127.0.0.2', 5556, [storeCaps1]), function() {
+                store.addAvailableNode(new models.Node('127.0.0.3', 5556, [storeCaps2]), function() {
+                    matcher.findNode(findCaps, function(node) {
+                        expect(node.host).to.equal('127.0.0.3');
+                        done();
+                    }, true);
+                });
+            });
+        });
+    });
 });
