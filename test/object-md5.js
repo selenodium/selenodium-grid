78e4aa6f1ff20e81a904885d425545fd5aa6192a
Sergey Belov <peimei@ya.ru>
2015-03-30 12:35:57 +0300

    Add tests for object-md5 module

---
+++ test/object-md5.js
@@ -0,0 +1,13 @@
+var objectMd5 = require('../lib/object-md5'),
+    expect = require('must');
+
+describe('object-md5', function() {
+    it('must calculate md5 hash for an empty object', function() {
+        expect(objectMd5({})).to.equal('d751713988987e9331980363e24189ce');
+    });
+
+    it('must calculate md5 hash for an object', function() {
+        expect(objectMd5({a: 1, b: 2})).to.equal('eda948b2ebf3ca8685938252dbc7fcdc');
+        expect(objectMd5({b: 2, a: 1})).to.equal('eda948b2ebf3ca8685938252dbc7fcdc');
+    });
+});
