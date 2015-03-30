ad43060d42297ed9a11fd3f7c174816d8cf82138
Sergey Belov <peimei@ya.ru>
2015-03-30 12:09:31 +0300

    Add welcome servlet test

---
+++ test/servlets/welcome.js
@@ -0,0 +1,27 @@
+var server = require('../../lib/server'),
+    config = require('../../lib/config'),
+    Registry = require('../../lib/registry'),
+    supertest = require('q-supertest');
+
+describe('servlets/welcome', function() {
+    var app, tester;
+    before(function() {
+        return server(new Registry(config())).listen(0)
+            .then(function(server) {
+                app = server;
+                tester = supertest(server);
+            });
+    });
+
+    after(function() {
+        return app.destroy();
+    });
+
+	describe('GET /', function() {
+		it('must respond with welcome message', function() {
+            return tester
+                .get('/')
+                .expect(200, /welcome/i);
+		});
+	});
+});
