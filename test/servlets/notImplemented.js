87e2316bffb99cd388a233037b7129fd2348aa76
Sergey Belov <peimei@ya.ru>
2015-03-30 12:09:42 +0300

    Add notImplemented servlet test

---
+++ test/servlets/notImplemented.js
@@ -0,0 +1,27 @@
+var server = require('../../lib/server'),
+    config = require('../../lib/config'),
+    Registry = require('../../lib/registry'),
+    supertest = require('q-supertest');
+
+describe('servlets/notImplemented', function() {
+    var app, tester;
+    before(function() {
+        return server(new Registry(config())).listen(0)
+            .then(function(server) {
+                app = server;
+                tester = supertest(server);
+            });
+    });
+
+    after(function() {
+        return app.destroy();
+    });
+
+	describe('GET /grid/driver', function() {
+		it('must respond with not implemented', function() {
+            return tester
+                .get('/grid/driver')
+                .expect(501, /not implemented/i);
+		});
+	});
+});
