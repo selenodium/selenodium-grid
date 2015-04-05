54ce58faa773796d19db28b997ea0c95b664ecfa
Sergey Belov <peimei@ya.ru>
2015-04-05 17:41:43 +0300

    Fix luster.conf.js

--- luster.conf.js
+++ luster.conf.js
@@ -1,15 +1,19 @@
+'use strict';
+
 module.exports = {
     app: './bin/selenodium-grid',
     workers: 1,
     server: {
         port: process.env.PORT || 4444
     },
-    //debug: {
-    //    port: 5010
-    //},
+    /*
+    debug: {
+        port: 5010
+    },
+    */
     extensions: {
         'luster-guard': {
-            patterns: ['index.js', 'server.js', 'lib/**/*.js', 'node_modules/**']
+            patterns: ['bin/selenodium-grid', 'lib/**/*.js', 'node_modules/**']
         }
     }
 };
